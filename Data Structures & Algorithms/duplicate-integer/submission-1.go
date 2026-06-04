bd5a98b4581b5a91f4c60834acb675ecc0c351f0
NikitaMushchak <39431136+NikitaMushchak@users.noreply.github.com>
2026-06-04 17:39:25 +0300

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.go
@@ -0,0 +1,12 @@
+func hasDuplicate(nums []int) bool {
+    m:= make(map[int]bool)
+
+    for _, num := range nums {
+        if m[num] {
+            return true
+        } else {
+            m[num] = true
+        }
+    }
+    return false
+}
