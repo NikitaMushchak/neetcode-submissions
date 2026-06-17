35f71f58e124c361d3a8f296de0a30849abc9b9c
NikitaMushchak <39431136+NikitaMushchak@users.noreply.github.com>
2026-06-17 14:18:02 +0300

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> out;
+        // sort the nums
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    out.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    // check the left dublicates
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    //[-4, -1, -1, 0, 1, 2]
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return out;
+    }
+};
