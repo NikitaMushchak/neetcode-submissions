81e44d964857b383100bb21edc666e4c43718f18
NikitaMushchak <39431136+NikitaMushchak@users.noreply.github.com>
2026-06-19 15:18:34 +0300

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.empty()) {
+            return 0;
+        }
+        // brute force is to get all posible 
+        // substrings put them in the vector and return 
+        // the largest one
+        vector<set<char>> vecOfSet(s.size()); 
+        for(size_t i = 0; i < s.size(); ++i) {
+            auto & charSet = vecOfSet[i];
+            size_t j = i;
+            while (j < s.size()) {
+                cout << "char[" << j << "]: " << s[j] << endl;
+                if (charSet.count(s[j])) {
+                    break;
+                } else {
+                    charSet.emplace(s[j]);
+                    j++;
+                }
+            }
+
+        }
+
+        int longest = 0;
+        for (const auto & charSet: vecOfSet) {
+            cout << "size: " << charSet.size() << endl;
+            longest = max(longest, (int)charSet.size());
+        }
+        return longest;
+    }
+};
